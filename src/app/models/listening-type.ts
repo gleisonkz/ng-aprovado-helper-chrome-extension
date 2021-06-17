beedc2fc943125bf3690342ebe97215eb38007f7
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-06-17 16:52:41 -0300

    📦 refactor: extract types to file

---
+++ src/app/models/listening-type.ts
@@ -0,0 +1,11 @@
+interface ListeningType {
+  key: number;
+  value: string;
+}
+
+enum eListeningType {
+  Active = 9178637,
+  Passive = 9178639,
+}
+
+export { ListeningType, eListeningType };
---
+++ src/app/models/video-info.ts
@@ -0,0 +1,6 @@
+export interface VideoInfo {
+  title: string;
+  currentDurationInSeconds: number;
+  totalDurationInSeconds: number;
+  totalDurationText: string;
+}
